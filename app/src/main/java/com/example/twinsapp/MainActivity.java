3a081247d2fdfe19d509ddd3c5225efcbe9ba74a
rpatten1 <rpatten1@css.edu>
2023-10-19 11:42:47 -0500

    Sprint 1, basic visual features and button usages

--- app/src/main/java/com/example/twinsapp/MainActivity.java
+++ app/src/main/java/com/example/twinsapp/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.twinsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonCalendar;
+    TextView textViewStatus;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewStatus = findViewById(R.id.textViewStatus);
+
+        setupButtonCalendar();
     }
+
+    private void setupButtonCalendar(){
+
+        buttonCalendar = findViewById(R.id.buttonCalendar);
+        buttonCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewStatus.setText("Code should set a calendar event for performance 1");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "Twins Opening Day");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Target Field");
+                GregorianCalendar begDate = new GregorianCalendar(2024, 3, 28, 15, 10);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                startActivity(calIntent);
+            }
+        });
+
+
+
+
+    }
+
+
+
 }
